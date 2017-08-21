0955e53e25e9744b4f6c0cdba6e95bd139db7faf
Ashish Joseph <v-asjoseph@expedia.com>
2017-08-21 23:37:35 +0530

    Adding some real user data

--- app/components/user-list.js
+++ app/components/user-list.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     moreDetails(user) {
-      console.log("Show more details of "+user);
+      console.log("Age of "+user.firstName+" is "+user.age);
     }
   }
 });
--- app/routes/user.js
+++ app/routes/user.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ['Ashish', 'Tina', 'Nikhil', 'Nimi'];
+    return [
+      {firstName:'Ashish', lastName:'Joseph', age:27},
+      {firstName:'Tina', lastName:'Bastin', age:40},
+      {firstName:'Nikhil', lastName:'K', age:32},
+      {firstName:'Nimi', lastName:'Manoharan', age:26}];
   }
 });
